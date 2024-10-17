eb3b525243bbed4b332eeeb6e1173af859715338
Loreks19 <brawl.boy444@gmail.com>
2024-10-17 21:44:58 +0300

    lab06

---
+++ lab006/android/app/src/main/kotlin/com/lntu/l06/lab006/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.lntu.l06.lab006
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lab006/lib/widgets/reset_password_screen.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+class ResetPasswordScreen extends StatelessWidget {
+  const ResetPasswordScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Autorization page"),
+        ),
+        body: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Center(
+                  child: Image.network(
+                    "https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Google-flutter-logo.svg/1024px-Google-flutter-logo.svg.png",
+                    width: 200,
+                  ),
+                ),
+                const SizedBox(height: 24),
+                const Center(
+                  child: Text(
+                    "Reset",
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blueGrey,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 24),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: "Reset",
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                const SizedBox(height: 24),
+                Row(
+                  children: [
+                    Expanded(child: SizedBox(
+                      height: 48,
+                      child: ElevatedButton(
+                          onPressed: () => {
+                            showDialog(
+                              context:context,
+                              builder: (BuildContext ctx){
+                                return const AlertDialog(
+                                  title: Text("Message"),
+                                  content: Text("Successful reset"),
+                                );
+                              },
+                            ),
+                          }, child: const Text("Reset")),
+                    ),
+                    ),
+                    const SizedBox(width: 16.0),
+                  ],
+                ),
+                const SizedBox(height: 24),
+                Row(
+                  children: [
+                    Expanded(
+                      child: SizedBox(
+                          height: 48,
+                          child:ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.white,
+                                foregroundColor: Colors.black,
+                              ),
+                              onPressed: () => {
+                                Navigator.pop(context),
+                              }, child: const Text("Back"))
+                      ),
+                    ),
+                    const SizedBox(width: 16.0),
+                  ],
+                ),
+              ],
+            )
+        )
+    );
+  }
+}
